05336a51eec4338e942395330e73a4c3fbc20aca
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-05-15 00:54:47 +0600

    Try to jwt functionality

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middle ware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173','http://localhost:5174'
+  ],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jm3t3oc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri);
@@ -21,6 +29,34 @@ const client = new MongoClient(uri, {
   }
 });
 
+// Middle wire
+const logger = async (req, res, next) => {
+  // console.log('called:', req.host, req.originalUrl);
+  console.log('log info: ', req.method, req.url);
+  next();
+}
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('token in the middleware: ', token);
+  // next()
+  // No token available
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // error
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    // if token is valid then it would be decoded
+    console.log('Value in the token ', decoded);
+    req.user = decoded;
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -29,7 +65,36 @@ async function run() {
     const assignmentCollection = client.db("onlinegroupstudyDB").collection("assignments");
     const submitCollection = client.db("onlinegroupstudyDB").collection("submitassignments");
     const userCollection = client.db("onlinegroupstudyDB").collection("users");
+    const featureCollection = client.db("onlinegroupstudyDB").collection("features");
 
+    // auth related api jwt
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '16' })
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      })
+        .send({ success: true })
+    })
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log('login out user: ', user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+    })
+
+
+    // feature related api
+    app.get('/features', async (req, res) => {
+      const cursor = featureCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    // assignments related api
     app.get('/assignments', async (req, res) => {
       const cursor = assignmentCollection.find();
       const result = await cursor.toArray();
@@ -87,6 +152,10 @@ async function run() {
     })
 
     app.get('/submits', async (req, res) => {
+      // console.log('token owner info: ', req.user);
+      // if(req.user.email !== req.query.email){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
       const result = await submitCollection.find().toArray();
       res.send(result)
     })
@@ -122,8 +191,9 @@ async function run() {
     })
 
     // spoecifiq student submited assignment
-    app.get('/submits/:email', async (req, res) => {
-      console.log(req.query.email);
+    app.get('/submits/:email',logger,verifyToken, async (req, res) => {
+      console.log('submit token: ', req.query.email);
+      console.log('cook cookies--2: ',req.cookies);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email }
@@ -140,7 +210,7 @@ async function run() {
     })
 
     // create user
-    app.post('/users',async(req,res)=>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user);
